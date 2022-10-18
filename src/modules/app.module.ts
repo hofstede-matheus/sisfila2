59d4e69f8faf5966c57ed3019b6bf8502610ed70
hofstede-matheus <hofstede.matheus@gmail.com>
2022-10-18 15:19:23 -0300

    chore: build clean architecture base folder schema

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../presentation/controllers/app.controller';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
