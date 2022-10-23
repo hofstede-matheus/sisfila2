7063474fb740b65ff3d256229cdde0b6ad64da82
hofstede-matheus <hofstede.matheus@gmail.com>
2022-10-23 13:49:54 -0300

    chore: make changes in dockerfile and docker-compose file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
+const API_PORT = process.env.API_PORT ?? 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -19,6 +21,8 @@ async function bootstrap() {
   );
   SwaggerModule.setup('docs', app, document, {});
 
-  await app.listen(3000);
+  await app.listen(API_PORT, () => {
+    console.log(`API running on port ${API_PORT} ✨`);
+  });
 }
 bootstrap();
