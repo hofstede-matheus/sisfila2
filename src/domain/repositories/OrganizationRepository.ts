8563a6dafad791d0e19825c695eb3d5e044ea072
hofstede-matheus <hofstede.matheus@gmail.com>
2022-11-02 18:26:49 -0300

    feat(Organizations: findOneOrAll): implement usecase

--- src/domain/repositories/OrganizationRepository.ts
+++ src/domain/repositories/OrganizationRepository.ts
@@ -3,7 +3,7 @@ import { OrganizationEntity } from '../entities/Organization.entity';
 export interface OrganizationRepository {
   create(name: string, code: string): Promise<string>;
   findByCode(code: string): Promise<OrganizationEntity | undefined>;
-  findOneByIdOrAll(id: string): Promise<OrganizationEntity[] | undefined>;
+  findOneByIdOrAll(id?: string): Promise<OrganizationEntity[] | undefined>;
   update(id: string, name?: string, code?: string): Promise<void>;
 }
 
--- src/interactors/usecases/FindOneOrAllOrganizationsUsecase.ts
+++ src/interactors/usecases/FindOneOrAllOrganizationsUsecase.ts
@@ -1,9 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { OrganizationEntity } from '../../domain/entities/Organization.entity';
+import { OrganizationNotFoundError } from '../../domain/errors';
 import { OrganizationRepository } from '../../domain/repositories/OrganizationRepository';
-import { Either } from '../../shared/helpers/either';
+import { Either, left, right } from '../../shared/helpers/either';
 import { DomainError } from '../../shared/helpers/errors';
 import { UseCase } from '../../shared/helpers/usecase';
+import { Validator } from '../../shared/helpers/validator';
 
 @Injectable()
 export class FindOneOrAllOrganizationsUsecase implements UseCase {
@@ -11,7 +13,17 @@ export class FindOneOrAllOrganizationsUsecase implements UseCase {
     @Inject(OrganizationRepository)
     private organizationRepository: OrganizationRepository,
   ) {}
-  execute(id?: string): Promise<Either<DomainError, OrganizationEntity[]>> {
-    throw new Error('Method not implemented.');
+  async execute(
+    id?: string,
+  ): Promise<Either<DomainError, OrganizationEntity[]>> {
+    const validation = Validator.validate({ id: [id] });
+    if (validation.isLeft()) return left(validation.value);
+
+    const organizations = await this.organizationRepository.findOneByIdOrAll(
+      id,
+    );
+
+    if (!organizations) return left(new OrganizationNotFoundError());
+    return right(organizations);
   }
 }
